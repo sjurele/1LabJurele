39af8cd03641edc4835df8f01429fe8d062368ee
sjurele <steponas.j@gmail.com>
2021-04-18 22:20:24 +0300

    Patobulinau kintamuju vardus bei padariau, kad paspaudus mygtuka, pasikeistu tekstas

--- app/src/main/java/com/example/a1labjurele/MainActivity.java
+++ app/src/main/java/com/example/a1labjurele/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.a1labjurele;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnClick(View view) {
+        this.tvMain.setText(R.string.txt_changed);
     }
 }
